a648e021f51f9d59916b21133304257374cb1a4d
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-08 18:45:14 -0500

    flying player

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetKeyDown(KeyCode.Space) ? jumpHeight : rb.velocity.y);
+	}
+}
