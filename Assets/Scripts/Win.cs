4110690c9222498292ad8a06db018d4289a39723
Nithead <nithead@yahoo.com>
2020-07-08 23:28:22 -0500

    win bar

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public string nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerControl player = collision.collider.gameObject.GetComponent<PlayerControl>();
+
+        if (player != null)
+        {
+            print("won");
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
