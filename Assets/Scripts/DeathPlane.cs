b7cf665fed6453ac28eacf61886f6c78d904578f
Nithead <nithead@yahoo.com>
2020-07-08 22:54:28 -0500

    death plane

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerControl player = collision.otherCollider.GetComponent<PlayerControl>();
+
+        if (player != null)
+        {
+            player.Die();
+        }
+    }
+}
